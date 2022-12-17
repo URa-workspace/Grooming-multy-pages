cff382f6bbcad1a7a07bca10222ecc817f5a23ca
URa-workspace <105509725+URa-workspace@users.noreply.github.com>
2022-12-17 19:15:19 +0200

    added app

---
+++ assets/data.js
@@ -0,0 +1,489 @@
+const imagesData = [
+  {
+    id: 1,
+    path: "dohliad-za-rotovoiu-porozhnynoiu.png",
+    alt: "догляд за ротовою порожниною",
+  },
+  {
+    id: 2,
+    path: "grooming-1.jpg",
+    alt: "стрижка овчарки 1",
+  },
+  {
+    id: 3,
+    path: "grooming-2.jpg",
+    alt: "стрижка овчарки 2",
+  },
+  {
+    id: 4,
+    path: "grooming-3.jpg",
+    alt: "стрижка овчарки 3",
+  },
+  {
+    id: 5,
+    path: "grooming-4.jpg",
+    alt: "грумінг 4",
+  },
+  {
+    id: 6,
+    path: "grooming-5.jpg",
+    alt: "грумінг 5",
+  },
+  {
+    id: 7,
+    path: "grooming-6.jpg",
+    alt: "грумінг 6",
+  },
+  {
+    id: 8,
+    path: "grooming-7.jpg",
+    alt: "грумінг 7",
+  },
+  {
+    id: 9,
+    path: "grooming-cat-1.jpg",
+    alt: "грумінг котов 1",
+  },
+  {
+    id: 10,
+    path: "grooming-dog-1.png",
+    alt: "грумінг собак 1",
+  },
+  {
+    id: 11,
+    path: "grooming-dog-2.png",
+    alt: "грумінг собак 2",
+  },
+  {
+    id: 12,
+    path: "grooming-double-1.jpg",
+    alt: "грумінг собак 3",
+  },
+  {
+    id: 13,
+    path: "grooming-double-2.jpg",
+    alt: "грумінг собак 4",
+  },
+  {
+    id: 14,
+    path: "grooming-double-3.jpg",
+    alt: "грумінг собак 5",
+  },
+  {
+    id: 15,
+    path: "grooming-double-4.jpg",
+    alt: "грумінг собак 6",
+  },
+  {
+    id: 16,
+    path: "grooming-double-5.jpg",
+    alt: "грумінг собак 7",
+  },
+  {
+    id: 17,
+    path: "grooming-kotov.png",
+    alt: "грумінг котов",
+  },
+  {
+    id: 18,
+    path: "grooming-kotov2.png",
+    alt: "грумінг котов 2",
+  },
+  {
+    id: 19,
+    path: "grooming-kotov3.png",
+    alt: "грумінг котов 3",
+  },
+  {
+    id: 20,
+    path: "grooming-kotov4.png",
+    alt: "грумінг котов 4",
+  },
+  {
+    id: 21,
+    path: "grooming-kotov5.png",
+    alt: "грумінг котов 5",
+  },
+  {
+    id: 22,
+    path: "grooming-kotov6.png",
+    alt: "грумінг котов 6",
+  },
+  {
+    id: 23,
+    path: "grooming-kotov7.png",
+    alt: "грумінг котов 7",
+  },
+  {
+    id: 24,
+    path: "grooming-kotov-sfinks.png",
+    alt: "грумінг котов сфинкс",
+  },
+  {
+    id: 25,
+    path: "kit2.png",
+    alt: "кіт 2",
+  },
+  {
+    id: 26,
+    path: "kit3.png",
+    alt: "кіт 3",
+  },
+  {
+    id: 27,
+    path: "pes.png",
+    alt: "пес",
+  },
+  {
+    id: 28,
+    path: "pes2.png",
+    alt: "пес 2",
+  },
+  {
+    id: 29,
+    path: "pes3.png",
+    alt: "пес 3",
+  },
+  {
+    id: 30,
+    path: "pes4.png",
+    alt: "пес 4",
+  },
+  {
+    id: 31,
+    path: "pes5.png",
+    alt: "пес 5",
+  },
+  {
+    id: 32,
+    path: "pes6.png",
+    alt: "пес 6",
+  },
+  {
+    id: 33,
+    path: "pes7.png",
+    alt: "пес 7",
+  },
+  {
+    id: 34,
+    path: "pes8.png",
+    alt: "пес 8",
+  },
+  {
+    id: 35,
+    path: "pes9.png",
+    alt: "пес 9",
+  },
+  {
+    id: 36,
+    path: "pes10.png",
+    alt: "пес 10",
+  },
+  {
+    id: 37,
+    path: "pes11.png",
+    alt: "пес 11",
+  },
+  {
+    id: 38,
+    path: "pes12.png",
+    alt: "пес 12",
+  },
+  {
+    id: 39,
+    path: "pes13.png",
+    alt: "пес 13",
+  },
+  {
+    id: 40,
+    path: "pes14.png",
+    alt: "пес 14",
+  },
+  {
+    id: 41,
+    path: "pes15.png",
+    alt: "пес 15",
+  },
+  {
+    id: 42,
+    path: "pes16.png",
+    alt: "пес 16",
+  },
+  {
+    id: 43,
+    path: "pes17.png",
+    alt: "пес 17",
+  },
+  {
+    id: 44,
+    path: "pes18.png",
+    alt: "пес 18",
+  },
+  {
+    id: 45,
+    path: "pes19.png",
+    alt: "пес 19",
+  },
+  {
+    id: 46,
+    path: "pes20.png",
+    alt: "пес 20",
+  },
+  {
+    id: 47,
+    path: "pes21.png",
+    alt: "пес 21",
+  },
+  {
+    id: 48,
+    path: "pes22.png",
+    alt: "пес 22",
+  },
+  {
+    id: 49,
+    path: "pes23.png",
+    alt: "пес 23",
+  },
+  {
+    id: 50,
+    path: "pes24.png",
+    alt: "пес 24",
+  },
+  {
+    id: 51,
+    path: "pes25.png",
+    alt: "пес 25",
+  },
+  {
+    id: 52,
+    path: "pes26.png",
+    alt: "пес 26",
+  },
+  {
+    id: 53,
+    path: "pes27.png",
+    alt: "пес 27",
+  },
+  {
+    id: 54,
+    path: "pes28.png",
+    alt: "пес 28",
+  },
+  {
+    id: 55,
+    path: "pes29.png",
+    alt: "пес 29",
+  },
+  {
+    id: 56,
+    path: "pes30.png",
+    alt: "пес 30",
+  },
+  {
+    id: 57,
+    path: "pes31.png",
+    alt: "пес 31",
+  },
+  {
+    id: 58,
+    path: "pes32.png",
+    alt: "пес 32",
+  },
+  {
+    id: 59,
+    path: "pes33.png",
+    alt: "",
+  },
+  {
+    id: 60,
+    path: "pes34.png",
+    alt: "пес 34",
+  },
+  {
+    id: 61,
+    path: "pes35.png",
+    alt: "пес 35",
+  },
+  {
+    id: 62,
+    path: "pes36.png",
+    alt: "пес 36",
+  },
+  {
+    id: 63,
+    path: "pes37.png",
+    alt: "пес 37",
+  },
+  {
+    id: 64,
+    path: "pes38.png",
+    alt: "пес 38",
+  },
+  {
+    id: 65,
+    path: "pes39.png",
+    alt: "пес 39",
+  },
+  {
+    id: 66,
+    path: "pes40.png",
+    alt: "пес 40",
+  },
+  {
+    id: 67,
+    path: "pes41.png",
+    alt: "пес 41",
+  },
+  {
+    id: 68,
+    path: "pes42.png",
+    alt: "пес 42",
+  },
+  {
+    id: 69,
+    path: "pes43.png",
+    alt: "пес 43",
+  },
+  {
+    id: 70,
+    path: "pes44.png",
+    alt: "пес 44",
+  },
+  {
+    id: 71,
+    path: "pes45.png",
+    alt: "пес 45",
+  },
+  {
+    id: 72,
+    path: "pes46.png",
+    alt: "пес 46",
+  },
+  {
+    id: 73,
+    path: "pes47.png",
+    alt: "пес 47",
+  },
+  {
+    id: 74,
+    path: "pes48.png",
+    alt: "пес 48",
+  },
+  {
+    id: 75,
+    path: "pes49.png",
+    alt: "пес 49",
+  },
+  {
+    id: 76,
+    path: "pes50.png",
+    alt: "пес 50",
+  },
+  {
+    id: 77,
+    path: "pes51.png",
+    alt: "пес 51",
+  },
+  {
+    id: 78,
+    path: "pes52.png",
+    alt: "пес 52",
+  },
+  {
+    id: 79,
+    path: "pes53.png",
+    alt: "пес 53",
+  },
+  {
+    id: 80,
+    path: "pes54.png",
+    alt: "пес 54",
+  },
+  {
+    id: 81,
+    path: "pes55.png",
+    alt: "пес 55",
+  },
+  {
+    id: 82,
+    path: "pes56.png",
+    alt: "пес 56",
+  },
+  {
+    id: 83,
+    path: "pes57.png",
+    alt: "пес 57",
+  },
+  {
+    id: 84,
+    path: "pes58.png",
+    alt: "пес 58",
+  },
+  {
+    id: 85,
+    path: "pes59.png",
+    alt: "пес 59",
+  },
+  {
+    id: 86,
+    path: "pes60.png",
+    alt: "пес 60",
+  },
+  {
+    id: 87,
+    path: "pes61.png",
+    alt: "пес 61",
+  },
+  {
+    id: 88,
+    path: "pes62.png",
+    alt: "пес 62",
+  },
+  {
+    id: 89,
+    path: "pes63.png",
+    alt: "пес 63",
+  },
+  {
+    id: 90,
+    path: "pes-i-vlada.png",
+    alt: "пес і Влада",
+  },
+  {
+    id: 91,
+    path: "pes-i-vlada2.png",
+    alt: "пес і Влада 2",
+  },
+  {
+    id: 92,
+    path: "stryjka-sobak1.png",
+    alt: "стрижка собак 1",
+  },
+  {
+    id: 93,
+    path: "stryjka-sobak2.png",
+    alt: "стрижка собак 2",
+  },
+  {
+    id: 94,
+    path: "stryjka-sobak3.png",
+    alt: "стрижка собак 3",
+  },
+  {
+    id: 95,
+    path: "stryjka-sobak4.png",
+    alt: "стрижка собак 4",
+  },
+  {
+    id: 96,
+    path: "stryjka-sobak5.png",
+    alt: "стрижка собак 5",
+  },
+  {
+    id: 97,
+    path: "stryjka-sobak6.png",
+    alt: "стрижка собак 6",
+  },
+];
+
+export default imagesData
